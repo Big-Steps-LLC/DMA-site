c4c7be46d59bac19d0b15102b764b2c36ea72e5f
Colton Curtis <coltoncurtis@Coltons-Air.attlocal.net>
2024-02-06 19:49:50 -0500

    hello

--- Backend/server.js
+++ Backend/server.js
@@ -1,33 +1,40 @@
+// Import required modules
 require('dotenv').config();
-
+const https = require('https');
+const fs = require('fs');
 const express = require('express');
 const sgMail = require('@sendgrid/mail');
 const bodyParser = require('body-parser');
+
+// Create express app
 const app = express();
-const port = process.env.PORT || 3001;
-const key = process.env.SENDGRID_API_KEY;
 
+// Set up middleware
 app.use(express.json());
-
 app.use(bodyParser.json());
-sgMail.setApiKey(key);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const phrase = process.env.TEST;
 
+// Load SSL/TLS certificate and private key
+const privateKeyPath = './certs/private-key.pem';
+const certificatePath = './certs/certificate.pem';
+const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+const certificate = fs.readFileSync(certificatePath, 'utf8');
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  passphrase: phrase 
+};
 
-  
-
+// Define routes
 app.post('/send-email', async (req, res) => {
     const formData = req.body;
-
-
-    // Create an HTML template dynamically based on formData
     const htmlBody = `
       <p>Name: ${formData.name}</p>
       <p>Email: ${formData.email}</p>
       <p>Subject: ${formData.subject}</p>
       <p>Message: ${formData.message}</p>
     `;
-
-    // Email data for sending
     const data = {
         to: 'bigstepllc1@gmail.com',
         from: 'bigstepllc1@gmail.com',
@@ -35,7 +42,6 @@ app.post('/send-email', async (req, res) => {
         text: 'This is a plain text version of the email body.',
         html: htmlBody,
     };
-
 
     try {
         await sgMail.send(data);
@@ -49,9 +55,11 @@ app.post('/send-email', async (req, res) => {
     }
 });
 
+// Define port for HTTPS server
+const httpsPort = process.env.HTTPS_PORT || 443;
 
-
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+// Start HTTPS server
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(httpsPort, () => {
+    console.log(`HTTPS server is running on port ${httpsPort}`);
 });
